cc047eca5f06f8e8ba056cbbb16257619a856a38
Yuri <yurialvesyamada@hotmail.com>
2020-10-15 11:18:58 -0300

    Adicionar arquivos

---
+++ JS/Produtos.js
@@ -0,0 +1,32 @@
+function exibir_Categoria(categoria) {
+
+    let elementos = document.getElementsByClassName("box_produtos")
+    console.log(elementos);
+
+    for(var i=0; i<elementos.length; i++) {
+
+        console.log(elementos[i].id);
+        if(categoria == elementos[i].id)
+        elementos[i].style = "display:block";
+        else 
+        elementos[i].style = "display:none";
+    }
+    
+};
+
+     let exibir_todos = () => {
+     let elementos = document.getElementsByClassName("box_produtos")
+        
+         for(var i=0; i<elementos.length; i++) {
+             elementos[i].style = "display:block";
+           
+                
+         }  
+
+
+     };
+
+       
+     
+     
+      
