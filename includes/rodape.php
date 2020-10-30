007b23b0e32237db2fbb729cd746d68796231fc5
Yuri <yurialvesyamada@hotmail.com>
2020-10-29 21:39:36 -0300

    Fix Class para Id

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,10 +1,10 @@
  <!-- Ródape e Formas de pagamento -->
  <div>
-     <footer class="rodapé">
+     <footer id="rodapé">
          <p id="pagamento">Formas de Pagamento: </p>
          <img src="./img/Pagamento.png" width="30%">
          <hr class="lines">
-         <div class="InfoTec">&copy; InfoTec</div>
+         <div id="InfoTec">&copy; InfoTec</div>
      </footer>
  </div>
  <!-- Ródape e Formas de pagamento -->
