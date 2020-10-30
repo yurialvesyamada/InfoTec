e7d825ad449183e4bb7307046716f5264a1147a5
Yuri <yurialvesyamada@hotmail.com>
2020-10-29 21:30:39 -0300

    Identando páginas e criando Includes

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
  <?php require("includes/cabeçalho.php"); ?>
+
  <body class="backgroundpadrao">
-   <header>
-            <!--
-          <div class="news">
-                <br>
-                <img class="crown1" src="./img/crown.png">
-                <hr class="linhas"> 
-                <h2>Technology News</h2>  
-                <hr class="linhas">
-                <br><br><br><br><br><br><br><br><br><br><br><br>    
-          </div> 
-          <img src="./img/novologo.png">
-            -->
-     <div class="card">
-          <img src="./img/core-i9.jpg" alt="cards" style="width:25%">
-          <img src="./img/ryzen7.jpg" alt="cards" style="width:25%">
-          <img src="./img/radeon-rx.jpg" alt="cards" style="width:25%">
-          <img src="./img/memoria-ram.jpg" alt="cards" style="width:25%">
-     </div>  
-   </header>
-   <main>
-   <div> 
-        <img class="imagens_tela_inicial" src="./img/news_banner.png">
-        <img class="imagens_tela_inicial" src="./img/card.jpg">
-        <img class="imagens_tela_inicial" src="./img/back_amd.jpg"> 
-   </div> 
-   </main>
-   
-    <?php require("includes/rodape.php");?>
-   
+     <header>
 
+         <div class="card">
+             <img src="./img/core-i9.jpg" alt="cards" style="width:25%">
+             <img src="./img/ryzen7.jpg" alt="cards" style="width:25%">
+             <img src="./img/radeon-rx.jpg" alt="cards" style="width:25%">
+             <img src="./img/memoria-ram.jpg" alt="cards" style="width:25%">
+         </div>
+     </header>
+     <main>
+         <div>
+             <img class="imagens_tela_inicial" src="./img/news_banner.png">
+             <img class="imagens_tela_inicial" src="./img/card.jpg">
+             <img class="imagens_tela_inicial" src="./img/back_amd.jpg">
+         </div>
+     </main>
 
-
+     <?php require("includes/rodape.php");?>
