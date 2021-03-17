bbc1184b3e7a427e6edd80f68d6ea8b39794d857
Yuri <you@teste.com>
2021-03-17 18:55:59 -0300

    Update Footer

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,6 +1,11 @@
  </body>
+
+ <div class="footer-top">
+     <h1><a href="http://infotec.infinityfreeapp.com/" style="color: #17a2b8;"> Conheça nosso novo Site InfoTec 2021</a>
+     </h1>
+ </div>
  <footer class="footer">
-     &copy; InfoTec 2021
+     &copy; InfoTec 2020
  </footer>
 
  <script src="assets/js/produtos.js"></script>
