ba736bdfebad47199889a382ec747ad1a451f79f
Yuri Alves Yamada <yurialvesyamada@hotmail.com>
2021-07-19 12:24:32 -0300

    Update page produtos

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,6 +1,6 @@
 function exibir_Categoria(categoria) {
 
-    let elementos = document.getElementsByClassName("cards")
+    let elementos = document.getElementsByClassName("cards-items")
     console.log(elementos);
 
     for(var i=0; i<elementos.length; i++) {
@@ -15,7 +15,7 @@ function exibir_Categoria(categoria) {
 };
 
      let exibir_todos = () => {
-     let elementos = document.getElementsByClassName("cards")
+     let elementos = document.getElementsByClassName("cards-items")
         
          for(var i=0; i<elementos.length; i++) {
              elementos[i].style = "display:block";
--- produtos.php
+++ produtos.php
@@ -6,23 +6,23 @@
 </header>
 
 <main>
-    <div class="row">
-        <section class="categorias col-3">
-            <h2 class="p-5">Categorias</h2>
-            <ul>
-                <li onclick="exibir_todos()">Todos (23)</li>
-                <li onclick="exibir_Categoria('Notebook')">Notebooks</li>
-                <li onclick="exibir_Categoria('Placa de Vídeo')">Placa de Vídeo</li>
-                <li onclick="exibir_Categoria('Processadores')">Processadores</li>
-                <li onclick="exibir_Categoria('Memória RAM')">Memória RAM</li>
-                <li onclick="exibir_Categoria('Monitores')">Monitor</li>
-                <li onclick="exibir_Categoria('Pc Gamer')">Pc Gamer</li>
-                <li onclick="exibir_Categoria('SSD')">SSD</li>
-                <li onclick="exibir_Categoria('Headset Gamer')">Headset Gamer</li>
-            </ul>
-        </section>
+    <h2 class="p-1">Categorias</h2>
+    <section class="categorias">
+        <ul>
+            <li onclick="exibir_todos()">Todos (23)</li>
+            <li onclick="exibir_Categoria('Notebook')">Notebooks</li>
+            <li onclick="exibir_Categoria('Placa de Vídeo')">Placa de Vídeo</li>
+            <li onclick="exibir_Categoria('Processadores')">Processadores</li>
+            <li onclick="exibir_Categoria('Memória RAM')">Memória RAM</li>
+            <li onclick="exibir_Categoria('Monitores')">Monitor</li>
+            <li onclick="exibir_Categoria('Pc Gamer')">Pc Gamer</li>
+            <li onclick="exibir_Categoria('SSD')">SSD</li>
+            <li onclick="exibir_Categoria('Headset Gamer')">Headset Gamer</li>
+        </ul>
+    </section>
 
-        <section class="produtos col-9">
+    <div class="row">
+        <section class="produtos col-12">
             <?php
             $sql = "SELECT * FROM produto";
             $result = $conn->query($sql);
@@ -30,27 +30,28 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
             ?>
-                    <div class="cards col-4" id="<?php echo $row["categoria"]; ?>">
-                        <div class="card-header">
-                            <img src="./img/<?php echo $row["imagem"]; ?>" title=" <?php echo $row["descricao"]; ?>">
-                            <!-- <div class="avaliacoes">
-                                <img src="./img/avaliacoes.png" title="Conferir Avaliações" alt="Conferir Avaliações">
-                            </div> -->
-                        </div>
-                        <!--  <div class="card-body">
-                            <h3> <?php echo $row["descricao"]; ?> </h3>
-                            <p><del>R$<?php echo $row["preco"]; ?></del></p>
-                            <h4>R$<?php echo $row["preco_final"]; ?></h4>
-                        </div>
-
-                        <div class="card-footer">
-                            <div class="btn col-s-12">
-                                <a href="pedidos.php">
-                                    <button>Eu Quero</button>
-                                </a>
+                    <article class="cards-flex-container col-3">
+                        <div class="cards-items" id="<?php echo $row["categoria"]; ?>">
+                            <div class="card-header">
+                                <img src="./img/<?php echo $row["imagem"]; ?>" title=" <?php echo $row["descricao"]; ?>">
+                                <div class="avaliacoes p-5">
+                                    <img src="./img/avaliacoes.png" title="Conferir Avaliações" alt="Conferir Avaliações">
+                                </div>
                             </div>
-                        </div> -->
-                    </div>
+                            <div class="card-body">
+                                <h3> <?php echo $row["descricao"]; ?> </h3>
+                                <p><del>R$<?php echo $row["preco"]; ?></del></p>
+                                <h4>R$<?php echo $row["preco_final"]; ?></h4>
+                            </div>
+                            <div class="card-footer">
+                                <div class="card-button col-12">
+                                    <a href="pedidos.php">
+                                        <button>Eu Quero</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
 
 
             <?php
